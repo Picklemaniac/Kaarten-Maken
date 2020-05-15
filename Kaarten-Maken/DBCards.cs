7039bfe32d6c39c531ace9bc808a9a38d550b0eb
Bas Lemmen <PS197800@summacollege.nl>
2020-05-15 09:41:27 +0200

    Added MYSQL Packages

---
+++ Kaarten-Maken/DBCards.cs
@@ -0,0 +1,19 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaarten_Maken
+{
+    class DBCards
+    {
+        MySqlConnection _conn;
+        public DBCards()
+        {
+            string connectie = string.Concat("Server=sql7.freemysqlhosting.net;Database=sql7338721;Uid=sql7338721;Pwd=PMXUjM37mF;");
+            _conn = new MySqlConnection(connectie);
+        }
+    }
+}
