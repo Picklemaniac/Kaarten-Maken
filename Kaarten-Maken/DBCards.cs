82233ee9e9b0146193f9a2bd1b436b3e9d888f03
Bas Lemmen <PS197800@summacollege.nl>
2020-05-15 09:51:50 +0200

    Added request querries

--- Kaarten-Maken/DBCards.cs
+++ Kaarten-Maken/DBCards.cs
@@ -1,6 +1,7 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,34 @@ namespace Kaarten_Maken
         {
             string connectie = string.Concat("Server=sql7.freemysqlhosting.net;Database=sql7338721;Uid=sql7338721;Pwd=PMXUjM37mF;");
             _conn = new MySqlConnection(connectie);
+        }
+
+        public DataTable GetCardNormal(string kleur)
+        {
+            _conn.Open();
+            MySqlCommand show = _conn.CreateCommand();
+            show.CommandText = "select * from cards where categorie = 'school' and kleur = '@kleur'";
+            show.Parameters.AddWithValue("@kleur", kleur);
+            MySqlDataReader reader = show.ExecuteReader();
+
+            DataTable dvData = new DataTable();
+            dvData.Load(reader);
+            _conn.Close();
+            return dvData;
+        }
+
+        public DataTable GetCardStage(string kleur)
+        {
+            _conn.Open();
+            MySqlCommand show = _conn.CreateCommand();
+            show.CommandText = "select * from cards where categorie = 'stage' and kleur = '@kleur'";
+            show.Parameters.AddWithValue("@kleur", kleur);
+            MySqlDataReader reader = show.ExecuteReader();
+
+            DataTable dvData = new DataTable();
+            dvData.Load(reader);
+            _conn.Close();
+            return dvData;
         }
     }
 }
